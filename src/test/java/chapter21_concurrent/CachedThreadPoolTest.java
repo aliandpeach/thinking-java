9f7cfa45d6120c24e93c6214d5e4c3da057c54cd
Neng <yidao620@gmail.com>
2012-06-04 22:18:06 +0800

    2012/06/03 -- Java多线程机制  加油  ^_^

---
+++ src/test/java/chapter21_concurrent/CachedThreadPoolTest.java
@@ -0,0 +1,17 @@
+package chapter21_concurrent;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yidao
+ * Date: 12-6-4
+ * Time: 下午9:45
+ * CachedThreadPoolTest
+ */
+public class CachedThreadPoolTest {
+    @Test
+    public void testThreadPool() throws Exception {
+        CachedThreadPool.threadPool();
+    }
+}
