5207ffdba64531a3d3e7f0f7ffe78a8979634601
Neng <yidao620@gmail.com>
2012-06-05 22:33:16 +0800

    2012/06/03 -- Swing  ^_^

---
+++ src/test/java/chapter21_concurrent/JoiningTest.java
@@ -0,0 +1,17 @@
+package chapter21_concurrent;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yidao
+ * Date: 12-6-5
+ * Time: 下午8:38
+ * JoiningTest
+ */
+public class JoiningTest {
+    @Test
+    public void testJoin() throws Exception {
+        Joining.join();
+    }
+}
