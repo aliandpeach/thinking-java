43516b6b2e8b32af79cf90d87400452bcb38aa58
Xiong Neng <yidao620@gmail.com>
2013-01-14 08:56:27 +0800

    测试CyclicBarrier 和 Future

--- src/main/java/chapter21_concurrent/barrier/CyclicBarrierDemo.java
+++ src/main/java/chapter21_concurrent/barrier/CyclicBarrierDemo.java
@@ -15,9 +15,12 @@
  */
 package chapter21_concurrent.barrier;
 
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Description of this file.
@@ -27,30 +30,158 @@ import java.util.concurrent.Executors;
  * @since 13-1-10
  */
 class CyclicBarrierDemo {
+    private static final Logger log = LoggerFactory.getLogger(CyclicBarrierDemo.class);
+
+    /**
+     * 测试结果：
+     * 运行3000次
+     * CyclicBarrier栅栏         --> 15秒
+     * CompletionService线程池    --> 8秒
+     *
+     * @param args
+     */
     public static void main(String[] args) {
-        for (int i = 1; i <= 15000; i++) {
-            System.out.println("****************分割线，这是第${i}次测试*****************");
+//        testCyclic();
+        testFutureThreadPool();
+//        testThreadPool();
+    }
+
+    /**
+     * 测试CompletionService线程池
+     */
+    private static void testFutureThreadPool() {
+        log.info("------------开始测试了-------------");
+        for (int i = 1; i <= 3000; i++) {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    testCyclic();
+                    execFutureThreadPoolTask();
                 }
             }).start();
         }
     }
 
+    /**
+     * 测试CompletionService线程池
+     */
+    private static void testThreadPool() {
+        log.info("------------开始测试了-------------");
+        for (int i = 1; i <= 3000; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    execThreadPoolTask();
+                }
+            }).start();
+        }
+    }
+
+    /**
+     * 测试CyclicBarrier栅栏
+     */
     private static void testCyclic() {
-        long startTime = System.currentTimeMillis();
+        log.info("------------开始测试了-------------");
+        for (int i = 1; i <= 3000; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    execCyclicTask();
+                }
+            }).start();
+        }
+    }
+
+    private static void execFutureThreadPoolTask() {
+        ExecutorService threadPool = null;
+        try {
+            int taskNum = 6;
+            threadPool = Executors.newFixedThreadPool(taskNum);
+            List<Future<String>> futures = new ArrayList<Future<String>>(10);
+//            CompletionService<String> pool = new ExecutorCompletionService<String>(threadPool);
+
+            for (int i = 0; i < taskNum - 1; i++) {
+                final long expendTime = (i + 1) * 1000L;
+                futures.add(threadPool.submit(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        exec(expendTime);
+                        return "success";
+                    }
+                }));
+            }
+            futures.add(threadPool.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    execException(2000L);
+                    return "success";
+                }
+            }));
+
+            for (Future<String> future : futures) {
+                String result = future.get();
+            }
+            log.info("-------最后这个单个整体的任务结束了-------");
+        } catch (Exception e) {
+            log.error("-------取结果错误------");
+        } finally {
+            if (threadPool != null) {
+                threadPool.shutdown();
+            }
+        }
+    }
+
+    private static void execThreadPoolTask() {
+        ExecutorService threadPool = null;
+        try {
+            int taskNum = 6;
+            threadPool = Executors.newFixedThreadPool(taskNum);
+            CompletionService<String> pool = new ExecutorCompletionService<String>(threadPool);
+
+            for (int i = 0; i < taskNum - 1; i++) {
+                final long expendTime = (i + 1) * 1000L;
+                pool.submit(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        exec(expendTime);
+                        return "success";
+                    }
+                });
+            }
+            pool.submit(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    execException(2000L);
+                    return "success";
+                }
+            });
+
+            for (int i = 0; i < taskNum; i++) {
+                String result = pool.take().get();
+//                log.info("----最小单元功能返回结果：" + result);
+            }
+            log.info("-------最后这个单个整体的任务结束了-------");
+        } catch (Exception e) {
+            log.error("-------取结果错误------");
+        } finally {
+            if (threadPool != null) {
+                threadPool.shutdown();
+            }
+        }
+    }
+
+    private static void execCyclicTask() {
+//        long startTime = System.currentTimeMillis();
         int threadCount = 6;
         //障栅集合点(同步器)
         final CyclicBarrier barrier = new CyclicBarrier(threadCount + 1);
-        ExecutorService executorService = Executors.newCachedThreadPool(new NamedThreadPoolFactory("序列化测试-"));
+        ExecutorService executorService = Executors.newCachedThreadPool(new NamedThreadPoolFactory("序列化测试---"));
         for (int i = 0; i < 5; i++) {
+            final long expendTime = (i + 1) * 1000L;
             executorService.execute(new Runnable() {
                 @Override
                 public void run() {
                     try {
-                        exec();
+                        exec(expendTime);
                         barrier.await();
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -62,14 +193,14 @@ class CyclicBarrierDemo {
             @Override
             public void run() {
                 try {
-                    execException();
+                    execException(2000L);
                     try {
                         barrier.await();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 } catch (Exception e) {
-                    System.out.println("=============error=========${e}");
+                    log.error("=============error=========${e}");
                     e.printStackTrace();
                 }
             }
@@ -80,26 +211,27 @@ class CyclicBarrierDemo {
             e.printStackTrace();
         }
         executorService.shutdown();
-        long endTime = System.currentTimeMillis();
-        System.out.println("---Whole time is : " + (endTime - startTime) / 1000 + "秒-----");
-        System.out.println("--------------END ALL--------------");
+        log.info("--------每次的栅栏测试结束了---------");
+//        long endTime = System.currentTimeMillis();
+//        log.info("---Whole time is : " + (endTime - startTime) / 1000 + "秒-----");
+//        log.info("--------------END ALL--------------");
     }
 
 
-    private static void exec() throws Exception {
-        work();
+    private static void exec(long expendTime) throws Exception {
+        work(expendTime);
     }
 
-    private static void execException() throws Exception {
-        work();
-        throw new RuntimeException("^^^^^^^^^My RuntimeException ^^^^^^^^");
+    private static void execException(long expendTime) throws Exception {
+        work(expendTime);
+//        throw new RuntimeException("^^^^^^^^^My RuntimeException ^^^^^^^^");
     }
 
-    private static void work() throws Exception {
-        System.out.println("-------START-----当前线程id：${Thread.currentThread().id}，线程Name：${Thread.currentThread().name}");
-        System.out.println("---------开始休息100毫秒---------");
-        Thread.sleep(100L);
-        System.out.println("-------END------当前线程id：${Thread.currentThread().id}，线程Name：${Thread.currentThread().name}");
+    private static void work(long expendTime) throws Exception {
+//        log.info("-------START-----当前线程id：${Thread.currentThread().id}，线程Name：${Thread.currentThread().name}");
+//        log.info("---------开始休息100毫秒---------");
+        Thread.sleep(expendTime);
+//        log.info("-------END------当前线程id：${Thread.currentThread().id}，线程Name：${Thread.currentThread().name}");
     }
 
 }
