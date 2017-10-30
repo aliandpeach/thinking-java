df3cc22c79451bcadce9d9d648a265d4594cd106
yidao620c <yidao620@gmail.com>
2017-10-31 00:26:53 +0800

    Stream的测试类

---
+++ src/test/java/java8/chap3/LambdaTest.java
@@ -0,0 +1,7 @@
+package java8.chap3;
+
+/**
+ * lambda test
+ */
+public class LambdaTest {
+}
---
+++ src/test/java/java8/chap4/StreamTest.java
@@ -0,0 +1,32 @@
+package java8.chap4;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * StreamTest 2017/10/31.
+ */
+public class StreamTest {
+    private List<Dish> menus;
+    @Before
+    private void pre() {
+        menus = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", true, 350, Dish.Type.OTHER),
+                new Dish("season fruit", true, 120, Dish.Type.OTHER),
+                new Dish("pizza", true, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH) );
+    }
+
+    @Test
+    public void test1() {
+
+    }
+}
